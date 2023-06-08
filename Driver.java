030b7163707abf533381c9c8ab6140ac9cb2bcb7
fazam <fazam0616@hotmail.com>
2023-06-08 11:28:50 -0400

    Changed Polynomial class to new specifications, and implemented unfinished add/multiply classes

--- Driver.java
+++ Driver.java
@@ -3,14 +3,20 @@ public class Driver{
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
 		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
+		double[] co = {0,1,2,3};
+		Polynomial p1 = new Polynomial(c1,co);
 		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		double [] c02 = {0,1,2,3,4};
+		Polynomial p2 = new Polynomial(c2,c02);
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
 		if(s.hasRoot(1))
 			System.out.println("1 is a root of s");
 		else
 			System.out.println("1 is not a root of s");
+
+		Polynomial p3 = new Polynomial(new double[]{1,1,7},new double[]{2,3,0});
+		Polynomial p4 = new Polynomial(new double[]{1,2},new double[]{4,1});
+		Polynomial p5 = p3.multiply(p4);
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,29 +1,102 @@
+import java.util.Arrays;
+
 public class Polynomial{
 	private double[] coeff;
+	private double[] exponent;
 	public Polynomial(){
 		this.coeff = new double[]{0};
+		this.exponent = new double[]{0};
 	}
-	public Polynomial(double[] nums){
-		this.coeff = nums;
+	public Polynomial(double[] coeff, double[] exponent){
+		this.coeff = coeff;
+		this.exponent=exponent;
 	}
 
 	public Polynomial add(Polynomial other){
-		double[] newP = new double[Math.max(this.coeff.length,other.coeff.length)];
-		for (int i = 0;i<newP.length;i++){
-			if (i<this.coeff.length){
-				newP[i] += this.coeff[i];
+		double[] coeff = new double[(this.coeff.length+other.coeff.length)];
+		double[] expo = new double[coeff.length];
+		Polynomial longer,shorter;
+		
+		if (this.coeff.length >= other.coeff.length){
+			longer = this;
+			shorter = other;
+		}else{
+			longer=other;
+			shorter=this;
+		}
+		
+		for (int i = 0; i < expo.length; i++) {
+			expo[i] = -1;
+		}
+		int pos = 0;
+		for (int i = 0;i<longer.coeff.length;i++){
+			for (int j = 0; j < shorter.coeff.length; j++) {
+				if (longer.exponent[i] == shorter.exponent[j]){
+					coeff[pos] = longer.coeff[i]+shorter.coeff[j];
+					expo[pos] = longer.exponent[i];
+					break;
+				}
 			}
-			if (i<other.coeff.length){
-				newP[i] += other.coeff[i];
+			if (expo[pos] == -1){
+				coeff[pos] = longer.coeff[i];
+				expo[pos] = longer.exponent[i];
+			}
+			pos++;
+		}
+
+		boolean found = false;
+		for (int i = 0; i < shorter.coeff.length; i++) {
+			for (int j = 0; j < pos && !found; j++) {
+				if (shorter.exponent[i] == expo[j])
+					found = true;
+			}
+			if (!found) {
+				coeff[pos] = shorter.coeff[i];
+				expo[pos] = shorter.exponent[i];
+				pos++;
 			}
 		}
-		return new Polynomial(newP);
+
+		return new Polynomial(coeff,expo);
+	}
+
+	public Polynomial scale(double d){
+		double[] co = this.coeff.clone();
+		for (int i = 0; i < co.length; i++) {
+			co[i] *= d;
+		}
+
+		return new Polynomial(co,this.exponent.clone());
+	}
+
+	public Polynomial multiply(Polynomial other){
+		Polynomial output = new Polynomial();
+		for (int i = 0; i < this.coeff.length; i++) {
+			Polynomial cur = other.scale(this.coeff[i]);
+
+			for (int j = 0; j < cur.exponent.length; j++) {
+				cur.exponent[j] += this.exponent[i];
+			}
+//			System.out.println(Arrays.toString(cur.coeff));
+//			System.out.println(Arrays.toString(cur.exponent));
+//			System.out.println(Arrays.toString(output.coeff));
+//			System.out.println(Arrays.toString(output.exponent));
+//			System.out.println();
+			output = output.add(cur);
+//			System.out.println(Arrays.toString(output.coeff));
+//			System.out.println(Arrays.toString(output.exponent));
+//			System.out.println("----------");
+		}
+
+		System.out.println(Arrays.toString(output.coeff));
+		System.out.println(Arrays.toString(output.exponent));
+		return output;
 	}
 	
 	public double evaluate(double x){
 		double val = 0;
 		for(int i = 0;i<this.coeff.length;i++){
-			val += this.coeff[i]*Math.pow(x,i);
+			val += coeff[i]*Math.pow(x,exponent[i]);
 		}
 		return val;
 	}
